bc94654e3739bf0c16f26d9e62d49314d5c5a622
gabeeemoraes <bielantonio917@gmail.com>
2026-09-05 17:01:05 -0300

    Carregar camada responsiva do front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/global.css";
 import "./styles/dashboard.css";
 import "./styles/dashboard-shell.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
